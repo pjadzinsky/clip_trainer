aeb310ce9f14a060952675fe8bf01ae452ca575d
Pablo Jadzinsky <pablo@mousera.com>
2017-03-19 10:47:14 -0700

    WIP: Adding simple example, doesn't work yet

---
+++ keras_cnn.py
@@ -0,0 +1,33 @@
+""" keras_cnn.py
+
+
+"""
+import os
+import tempfile
+
+from keras.layers import Activation
+from keras.layers import Dense
+from keras.layers.convolutional import Conv3D
+from keras.layers.core import Flatten
+from keras.models import Sequential
+
+import utils
+
+model = Sequential()
+model.Add(Conv3D(3, 3, 1, "same", 32, input_dim=784))
+model.Add(Activation('relu'))
+model.Add(Flatten())
+model.Add(Dense(2))
+model.Add(Activation('softmax'))
+
+# For a binary classification problem
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model, iterating on the data in batches of 32 samples
+#filename = download_dataset_from_s3('clips.hdf5')
+features, labels = load_features_and_labels('test.hdf5')
+print(features.shape)
+print(labels.shape)
+model.fit(features, labels, epochs=2, batch_size=1)
---
+++ utils.py
@@ -0,0 +1,39 @@
+""" keras_cnn.py
+
+
+"""
+import os
+import tempfile
+
+import boto3
+import h5py
+
+import config
+
+AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
+BUCKET = config.BUCKET
+
+
+def download_dataset_from_s3(key):
+    assert(key.endswith('.hdf5'))
+
+    client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+    _, temp_hdf5 = tempfile.mkstemp(suffix='.hdf5')
+    client.download_file(BUCKET, key, temp_hdf5)
+
+    return temp_hdf5
+
+
+def load_features_and_labels(filename):
+    """ First use download_dataset to get the s3 file to local drive.
+    Then pass that file to this function to get 'features' and 'labels'
+    """
+    fid = h5py.File(filename, 'r')
+    features = fid['features'][:]
+    labels = fid['labels'][:]
+
+    return features, labels
+
+
